3139762b7fb58b511164ebc7161166e682fc43d3
ousttrue <ousttrue@gmail.com>
2023-06-30 18:46:10 +0900

    human tree

--- humanoid_properties.py
+++ humanoid_properties.py
@@ -1,4 +1,5 @@
 import bpy
+from typing import NamedTuple, List, Iterable, Optional
 
 PROP_NAMES = [
     "hips",
@@ -53,6 +54,136 @@ PROP_NAMES = [
     "right_little_intermediate",
     "right_little_distal",
 ]
+
+
+class Node(NamedTuple):
+    prop: str
+    children: List["Node"]
+
+
+def make_hand(prefix: str) -> Node:
+    return Node(
+        f"{prefix}hand",
+        [
+            Node(
+                f"{prefix}thumb_metacarpal",
+                [Node(f"{prefix}thumb_proximal", [Node(f"{prefix}thumb_distal", [])])],
+            ),
+            Node(
+                f"{prefix}index_proximal",
+                [
+                    Node(
+                        f"{prefix}index_intermediate",
+                        [Node(f"{prefix}index_distal", [])],
+                    )
+                ],
+            ),
+            Node(
+                f"{prefix}middle_proximal",
+                [
+                    Node(
+                        f"{prefix}middle_intermediate",
+                        [Node(f"{prefix}middle_distal", [])],
+                    )
+                ],
+            ),
+            Node(
+                f"{prefix}ring_proximal",
+                [
+                    Node(
+                        f"{prefix}ring_intermediate",
+                        [Node(f"{prefix}ring_distal", [])],
+                    )
+                ],
+            ),
+            Node(
+                f"{prefix}little_proximal",
+                [
+                    Node(
+                        f"{prefix}little_intermediate",
+                        [Node(f"{prefix}little_distal", [])],
+                    )
+                ],
+            ),
+        ],
+    )
+
+
+TREE = Node(
+    "hips",
+    [
+        Node(
+            "spine",
+            [
+                Node(
+                    "chest",
+                    [
+                        Node("neck", [Node("head", [])]),
+                        Node(
+                            "left_shoulder",
+                            [
+                                Node(
+                                    "left_upper_arm",
+                                    [Node("left_lower_arm", [make_hand("left_")])],
+                                )
+                            ],
+                        ),
+                        Node(
+                            "right_shoulder",
+                            [
+                                Node(
+                                    "right_upper_arm",
+                                    [Node("right_lower_arm", [make_hand("right_")])],
+                                )
+                            ],
+                        ),
+                    ],
+                )
+            ],
+        ),
+        Node(
+            "left_upper_leg",
+            [Node("left_lower_leg", [Node("left_foot", [Node("left_toes", [])])])],
+        ),
+        Node(
+            "right_upper_leg",
+            [Node("right_lower_leg", [Node("right_foot", [Node("right_toes", [])])])],
+        ),
+    ],
+)
+
+
+def get_node(prop: str) -> Optional[Node]:
+    def find(node):
+        if node.prop == prop:
+            return node
+        for child in node.children:
+            found = find(child)
+            if found:
+                return found
+
+    return find(TREE)
+
+
+def enum_children(prop: str) -> Iterable[str]:
+    found = get_node(prop)
+    if found:
+        for child in found.children:
+            yield child.prop
+
+
+def get_parent(prop: str) -> Optional[str]:
+    def find_parent(node) -> Optional[Node]:
+        for child in node.children:
+            if child.prop == prop:
+                return node
+            found = find_parent(child)
+            if found:
+                return found
+
+    found = find_parent(TREE)
+    if found:
+        return found.prop
 
 
 class HumanoidProperties(bpy.types.PropertyGroup):
